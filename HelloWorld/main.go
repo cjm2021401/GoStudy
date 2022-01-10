141c425855b5559ac4046e40718b2135614ffbb3
최정민 <>
2022-01-10 22:04:37 +0900

    [mayf]변수 생성 & 출력

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 //main 프로그램 시작점
+//;은 안붙이는게 아니라 이걸로 구분된 코드를 해석
 func main() {
-//{ <-오류 왜냐면 세미콜론이 붙어버림
-	//;은 안붙이는게 아니라 이걸로 구분된 코드를 해석
+	//일반적인 언어와 순서 반대로
+	//~은 ~이다로 기억하면 편함
+	var x int =10
+	var arr [5]int
+
+
+	fmt.Printf("변수 x: %d",x);
+	fmt.Printf("배열 첫번쨰 : %d",arr[0]);
 	fmt.Println("hello")
 }
